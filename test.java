011c72e06a592f883066998a60ecf83547d1bfd6
J4nN4w <88735678+J4nN4w@users.noreply.github.com>
2021-08-10 21:36:17 +0200

    Add files via upload

---
+++ test.java
@@ -0,0 +1,6 @@
+public class CPUCores {
+    public static void main(String[] args) {
+      int processors = Runtime.getRuntime().availableProcessors();
+      System.out.println("CPU cores: " + processors);
+    }
+}
